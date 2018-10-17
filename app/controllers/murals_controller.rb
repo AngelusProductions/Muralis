d3d3300384ebe836ee7e8d89a45f2d297e5f0ed2
Junaid Siddiqui <junaid.wim@gmail.com>
2018-10-17 09:47:25 -0400

    Created Mural submission form and tests

--- app/controllers/murals_controller.rb
+++ app/controllers/murals_controller.rb
@@ -1,5 +1,30 @@
 class MuralsController < ApplicationController
+  before_action :authenticate_user!, except: [:index,:show]
+
   def index
     @murals = Mural.all
+  end
+
+  def new
+    @mural = Mural.new
+  end
+
+  def create
+    @mural = Mural.new(mural_params)
+    @mural.user = current_user
+    if @mural.save
+      flash[:notice] = "Mural was successfully created"
+      redirect_to root_path
+    else
+      render new_mural_path
+    end
+
+  end
+
+
+  private
+
+  def mural_params
+    params.require(:mural).permit(:title,:photo,:description,:location)
   end
 end
