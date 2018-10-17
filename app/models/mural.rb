52ca770781d0cc463f0bee15eb132d7ad025401d
mrjoshdev <wyman.josh@gmail.com>
2018-10-17 16:08:54 -0400

    added ability to upload mural photo to AWS

--- app/models/mural.rb
+++ app/models/mural.rb
@@ -1,6 +1,8 @@
 class Mural < ApplicationRecord
   validates_presence_of :title, :description, :location, :photo, :upvotes, :downvotes
 
+  mount_uploader :photo, MuralPhotoUploader
+
   belongs_to :user
   has_many :reviews
 end
