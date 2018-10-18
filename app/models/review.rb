77b50bcb577f80852c754c111ea62ce65c3c2c36
Corey Angelus <angelusproductions@gmail.com>
2018-10-18 10:47:40 -0400

    Created model tests for mural and review; made small change to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   validates :rating, numericality: { greater_than: 0, less_than_or_equal_to: 10 }
 
-  belongs_to :mural, :user
+  belongs_to :mural
+  belongs_to :user
 end
