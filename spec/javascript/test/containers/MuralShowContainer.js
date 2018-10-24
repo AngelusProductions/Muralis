6227eadbb64a81a2041f87a9a234444e880e4476
Junaid Siddiqui <junaid.wim@gmail.com>
2018-10-24 17:31:13 -0400

    Made sure that Enzyme tests were compatible with new fetch calls

--- spec/javascript/test/containers/MuralShowContainer.js
+++ spec/javascript/test/containers/MuralShowContainer.js
@@ -9,6 +9,8 @@ import fetchMock from 'fetch-mock'
 describe('MuralShowContainer', () => {
   let wrapper;
   let mural;
+  let reviews;
+  let currentUser;
 
   beforeEach(() => {
     jasmineEnzyme();
@@ -19,7 +21,7 @@ describe('MuralShowContainer', () => {
       description: "She smiles",
       location: "The Louvre",
       photo: {
-        url: '/assets/funny_mona_lisa.jpg'
+        url: '../../../../spec/support/images/BasqWarhol.jpq'
       },
       upvotes: 0,
       downvotes: 0,
@@ -27,10 +29,48 @@ describe('MuralShowContainer', () => {
       updated_at: "",
       created_at: ""
     }
+    reviews = [
+      {
+        id: 1,
+        comment: "This mural was just okay",
+        rating: 5,
+        created_at: "",
+        updated_at: "",
+        mural_id: 1,
+        user_id: 1
+      },
+      {
+        id: 2,
+        comment: "This mural is incredible!",
+        rating: 10,
+        created_at: "",
+        updated_at: "",
+        mural_id: 1,
+        user_id: 1
+      }
+    ]
+    currentUser = {
+      id: 1,
+      first_name: "Junaid",
+      last_name: "Siddiqui",
+      user_photo: "",
+      username: "",
+      email: "google@gmail.com",
+      created_at: "",
+      updated_at: ""
+    }
 
     fetchMock.get(`/api/v1/murals/${mural.id}`, {
       status: 200,
       body: mural
+    });
+    fetchMock.get(`/api/v1/user`, {
+      status: 200,
+      body: currentUser
+    });
+    fetchMock.get(`/api/v1/reviews`, {
+      status: 200,
+      body: reviews
     });
 
     wrapper = mount(<MuralShowContainer
@@ -42,7 +82,7 @@ describe('MuralShowContainer', () => {
   afterEach(fetchMock.restore)
 
   it('should render state with murals object empty', () => {
-    expect(wrapper.state()).toEqual({ mural: {
+    expect(wrapper.state("mural")).toEqual({
         id: 0,
         title: "",
         description: "",
@@ -53,21 +93,21 @@ describe('MuralShowContainer', () => {
         upvotes: 0,
         downvotes: 0,
         user_id: 0,
-        updated_at: "",
-        created_at: ""
+        created_at: "",
+        updated_at: ""
         }
-      });
+      );
     })
 
   it('should render a Mural Component', () => {
     expect(wrapper.find(MuralShow)).toBePresent();
   });
-
+// syntax for testing props in implementing react tests/elephant
   it('should render correct text after the fetch call', (done) => {
     setTimeout( () => {
-      expect(wrapper.find('h1').text()).toMatch(mural.title)
-      expect(wrapper.find('h5').text()).toMatch(mural.location)
-      expect(wrapper.find('p').text()).toMatch(mural.description)
+      expect(wrapper.find('MuralShow').find('h1').text()).toMatch(mural.title)
+      expect(wrapper.find('MuralShow').find('h5').text()).toMatch(mural.location)
+      expect(wrapper.find('MuralShow').find('p').text()).toMatch(mural.description)
       done()
     }, 0)
   });
